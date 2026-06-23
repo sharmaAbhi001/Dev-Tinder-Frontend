4a48802da8a9e94efeb97996f3dcdc09bdafdb92
Abhishek Sharma <insanepanther97@gmail.com>
2026-06-23 14:40:04 +0530

    Update BASE_URL for production environment

--- src/utils/constent.jsx
+++ src/utils/constent.jsx
@@ -2,4 +2,4 @@
 export const _BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "https://connect.backend.codecampus.in";
+    : "https://tinder.codecampus.in/";
